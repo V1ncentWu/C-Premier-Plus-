c0ce2521e30a6aae99f1cf5c19c8562e6cb76a1a
Vingtcents <yhwu_26@163.com>
2022-05-03 20:28:19 +0800

    using escape sequence

---
+++ Chapter_3/bondini.cpp
@@ -0,0 +1,13 @@
+//bondini.cpp -- using escape sequence
+#include <iostream>
+int main()
+{
+    using namespace std;
+    cout << "\aOperation \"HyperHype\" is now activated!\n";
+    cout << "Enter your agent code: ________\b\b\b\b\b\b\b\b";
+    long code;
+//    cin >> code;
+    cout << "\aCode verified! Proceed with Plan Z3!\n";
+
+    return 0;
+}
